69c0514490c6ac79dbebbb553647a66ae793b915
Abhishek Gogoi <abhishekgogoi1997@gmail.com>
2022-06-02 18:10:32 +0530

    security-101 | Fresh Push

---
+++ src/main/java/com/security/application/HomeResource.java
@@ -0,0 +1,13 @@
+package com.security.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	@GetMapping("/")
+	public String home() {
+		return("<h1>WELCOME!!!</h1>");
+	}
+}
